d4042299cd8cc1199e4b362dcb687d111c5bb432
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-04-27 21:58:44 +0200

    metodos propiedades y plantillas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Hola Mundo';
+  title = 'app works!';
+  color: String = 'red';
+
+  generateRandom(): String {
+    return Math.floor(Math.random() * 255).toString(16);
+  }
+
+  colorHex(): String {
+    return (
+      '#' +
+      this.generateRandom() +
+      this.generateRandom() +
+      this.generateRandom()
+    );
+  }
 }
